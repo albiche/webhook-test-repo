8c6adeda28bd32705f7f5fca784502ec91400be8
albiche <alban.danet@efrei.net>
2024-05-27 11:29:19 +0200

    final

--- app.js
+++ app.js
@@ -6,13 +6,10 @@ const { handleGithubWebhook } = require('./github-webhook');
 const app = express();
 const PORT = config.port || 3000;
 
-// Middleware to parse JSON bodies
 app.use(bodyParser.json());
 
-// Webhook endpoint
 app.post('/webhook', handleGithubWebhook);
 
-// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
