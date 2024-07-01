403ae065acfaf474e74a5abe78a23c25dfcf2582
albiche <alban.danet@efrei.net>
2024-07-01 11:21:15 +0200

    ziz

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     port: 3000,
     githubSecret: '321839932004',
-    githubRepo: 'JaTadd/github-webhook',
+    githubRepo: 'albiche/webhook-test-repo',
     githubBranch: 'main'
 };
